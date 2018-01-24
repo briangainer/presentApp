1e0c8dcb7ab381dae24448e6b2865f0e422af09c
Brian <blgainer@yahoo.com>
2018-01-23 19:14:45 -0500

    form bug fixxed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,8 +30,8 @@ $(document).on('click', '.add_sections', function () {
     //get number of milliseconds since midnight Jan 1, 1970  
     //and use it for address key 
     var mSec = date.getTime();
-    var str = '<fieldset><label for="deck_sections_attributes_999999_title">Section</label><textarea name="deck[sections_attributes][999999][title]" id="deck_sections_attributes_999999_title"></textarea> <br>Time (seconds) <br><input type="number" name="deck[sections_attributes][999999][time]" id="deck_sections_attributes_999999_time" /><input type="hidden" value="false" name="deck[sections_attributes][999999][_destroy]" id="deck_sections_attributes_999999__destroy" /><a class="remove_sections" href="#">remove</a></fieldset>'
-    var hey = str.replace("999999", mSec);
+    var str = '<fieldset><label for="deck_sections_attributes_99_title">Section</label><textarea name="deck[sections_attributes][99][title]" id="deck_sections_attributes_99_title"></textarea> <br>Time (seconds) <br><input type="number" name="deck[sections_attributes][99][time]" id="deck_sections_attributes_99_time" /><input type="hidden" value="false" name="deck[sections_attributes][99][_destroy]" id="deck_sections_attributes_99__destroy" /><a class="remove_sections" href="#">remove</a></fieldset>'
+    var hey = str.replace(/99/g, mSec);
     $(this).before(hey)
 	event.preventDefault()
 });
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Deck.create(title: 'first deck')
-Deck.create(title: '2nd deck')
-Deck.create(title: '3rd deck')
-Deck.create(title: '4th deck')
-Deck.create(title: '5th deck')
-Deck.create(title: '6th deck')
+Deck.destroy_all
+Section.destroy_all
+
+a = Deck.create(title: 'first deck')
+b = Deck.create(title: '2nd deck')
+c = Deck.create(title: '3rd deck')
+d = Deck.create(title: '4th deck')
+e = Deck.create(title: '5th deck')
+f = Deck.create(title: '6th deck')
 
 15.times do |i|
-	Section.create(title: i.to_s, deck_id: 1)
+	Section.create(title: 'test' + i.to_s, deck_id: a.id, time: 17)
 end
 
 15.times do |i|
-	Section.create(title: i.to_s, deck_id: 2)
+	Section.create(title: 'test' + i.to_s, deck_id: c.id, time: 17)
 end
 
 15.times do |i|
-	Section.create(title: i.to_s, deck_id: 4)
+	Section.create(title: 'test' + i.to_s, deck_id: b.id, time: 17)
 end
